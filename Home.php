e4707c94e3e0b7338cb8f2bd037e81a89cb815df
Antoine <antoine_boucher@outlook.com>
2015-03-25 20:48:42 -0400

    up to date

---
+++ Home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <title>Projet Personnel d’Orientation(PPO)</title>
+		<!--Seo-->
+		<meta charset="UTF-8">
+		<meta name="author" content="Antoine Boucher">
+		<meta name="description" content="Antoine Boucher Projet">
+		<meta name="application-name"content="Antoine Boucher Website">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<script src="js/jquery-2.1.3.min.js"></script>
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+<script src="js/bootstrap/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.min.css">
+                             <!--Jquery-->
+
+        <script src="/js/plugins.js"></script>
+        <script src="/js/main.js"></script>
+
+        
+    </head>
+    <body>
+
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <!--Javascript -->
+
+        
+ <?php require("php/nav.php"); ?> 
+        
+      
+</body>
+</html>
